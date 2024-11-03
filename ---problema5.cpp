5ff7c92df11b1fed67a6011ec349709831bf6b1a
Jhonel <jgapazap@unjbg.edu.pe>
2024-11-02 20:58:25 -0500

    problema 11

---
+++ ---problema5.cpp
@@ -0,0 +1,13 @@
+//Múltiplo más pequeño
+//2520 es el número más pequeño que se puede dividir por cada uno de los números de 1 a 10 sin ningún resto.
+//¿Cuál es el número positivo más pequeño que es?divisible de manera uniformepor todos los números del 1 al 20
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int inferior=1;
+	int superior=10;
+	
+	return 0;
+}
---
+++ ---problema7.cpp
@@ -0,0 +1,13 @@
+//10001 numero primo
+//Enumerando los primeros seis números primos: 2,3,5,7,11,13 podemos ver que el 6to primo es 13
+//cual es el 10001vo numero primo
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int n=1001;
+	int i=3, k=1;
+	while()
+	return 0;
+}
